32b5ac400384b3428f1753d3cff358ff95032d8c
stefanqwerty <stefan.vanghelicescu@gmail.com>
2021-02-20 23:10:46 +0000

    20.02

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+       
+        public string Game(int ID)
+        {
+            return HttpUtility.HtmlEncode($"ID is {ID}");
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
